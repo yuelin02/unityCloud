2446ec4d66a1955c6d589fdcfc753dbecd41a106
yue lin(Christine) <liny498@newschool.edu>
2017-03-11 01:51:32 -0500

    first commit

---
+++ Dai/Assets/scripts/CameraOrbit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour {
+
+	public Transform target;
+	public float rotSpeed = 15f;
+
+//	public float rotSpeed = 0.5f;
+
+	public void MoveHorizontal(bool left){
+		float dir = 1;
+		if (!left)
+			dir *= -1;
+		transform.RotateAround (target.position, Vector3.up, 10*Time.deltaTime * rotSpeed * dir);
+	}
+	public void MoveVertical(bool up){
+		float dir = 1;
+		if (!up)
+			dir *= -1;
+		transform.RotateAround (target.position, transform.TransformDirection(Vector3.right), 10*Time.deltaTime * rotSpeed * dir);
+	}
+}
---
+++ Dai/Assets/scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	CameraOrbit cam;
+	private Touch initTouch = new Touch();
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<CameraOrbit>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+//			cam.MoveHorizontal (true);
+//		} else if(Input.GetKeyDown (KeyCode.RightArrow)){
+//			cam.MoveHorizontal (false);
+//		}else if(Input.GetKeyDown (KeyCode.UpArrow)){
+//			cam.MoveVertical (true);
+//		}else if(Input.GetKeyDown (KeyCode.DownArrow)){
+//			cam.MoveVertical (false);
+//		}
+
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase == TouchPhase.Began) {
+				initTouch = touch;
+			}else if(touch.phase == TouchPhase.Moved) {
+				float deltaX = initTouch.position.x - touch.position.x;
+				if (deltaX > 20) {
+					cam.MoveHorizontal (true);
+				} else if (deltaX < -20) {
+					cam.MoveHorizontal (false);
+				}
+			}
+		}
+	}
+}
